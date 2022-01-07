122d9b0f594958f3cd07ce09c8de61743a9785fe
Allen Zhong <allen@moe.cat>
2022-01-07 12:06:29 +0800

    support custom cri

--- tTBD.sh
+++ tTBD.sh
@@ -1,13 +1,16 @@
 git clone $pkggit /src/$pkgname-$pkgver
 cd /src/$pkgname-$pkgver
 git checkout $pkgcommit
+
 export SOURCE_DATE_EPOCH=$(git log -1 --format=%ct)
 build
-export pkgdir=/tmp/TBD-tmp
-mkdir $pkgdir
+
+export pkgdir=`mktemp -d`
 package
+
 tar --sort=name \
       --mtime="@${SOURCE_DATE_EPOCH}" \
       --owner=0 --group=0 --numeric-owner \
       --pax-option=exthdr.name=%d/PaxHeaders/%f,delete=atime,delete=ctime \
       -cf /publish/$pkgname-$pkgver.tar $pkgdir
+
