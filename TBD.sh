852df8a3c4d87b10795444f2492050bbe5b42d26
Allen Zhong <allen@atr.me>
2022-01-07 15:11:53 +0800

    cache git repo and package custom files

--- TBD.sh
+++ TBD.sh
@@ -1,10 +1,24 @@
 #!/bin/bash
+set -e
 source $1
+
+# update git cache
+cachedir=$(realpath $(dirname $1))/.cache/$pkgname
+mkdir -p $cachedir
+if [ -d $cachedir/git ]; then
+  opwd=$PWD
+  cd $cachedir/git
+  git fetch --all
+  cd $opwd
+else
+  git clone $pkggit $cachedir/git
+fi
 
 CRI="${TBD_CRI:-docker}"
 
 $CRI run -it \
     -v $(realpath $1):/src/pkgmeta \
+    -v $cachedir:/src/$pkgname \
     -v $(dirname "$(realpath "${BASH_SOURCE[0]}")")/iTBD.sh:/usr/bin/iTBD.sh \
     -v $PWD:/publish \
     $image \
