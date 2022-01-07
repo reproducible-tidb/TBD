1ab0736402105cae6fa23dd915025c7dce396338
nexustar <inexustar@gmail.com>
2022-01-07 12:00:30 +0800

    rename tTBD to iTBD

--- TBD.sh
+++ TBD.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 source $1
-docker run -it -v $(realpath $1):/src/pkgmeta -v $(dirname "$(realpath "${BASH_SOURCE[0]}")")/tTBD.sh:/usr/bin/tTBD.sh -v $PWD:/publish $image "bash" "-c" "source /src/pkgmeta && source /usr/bin/tTBD.sh"
+docker run -it -v $(realpath $1):/src/pkgmeta -v $(dirname "$(realpath "${BASH_SOURCE[0]}")")/iTBD.sh:/usr/bin/iTBD.sh -v $PWD:/publish $image "bash" "-c" "source /src/pkgmeta && source /usr/bin/iTBD.sh"
